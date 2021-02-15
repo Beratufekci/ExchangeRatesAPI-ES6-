0081c64e9152f9b02744aeeb64e55c42cec43616
DESKTOP-0FBHRAR\DELL <berat.tufekci@kafein.com.tr>
2021-02-15 22:48:16 +0300

    User Interface

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const amountElement = document.querySelector("#amount");
 const firstSelect = document.querySelector("#firstCurrency");
 const secondSelect = document.querySelector("#secondCurrency");
 const currency = new Currency("USD","TRY");
+const ui = new UI(firstSelect,secondSelect);
 
 eventListeners();
 
@@ -9,15 +10,19 @@ function eventListeners(){
     amountElement.addEventListener("input",exchangeCurrency);
     firstSelect.onchange = function(){
         currency.changeFirstCurrency(firstSelect.options[firstSelect.selectedIndex].textContent); 
+        ui.changeFirst();
     };
     secondSelect.onchange = function(){
         currency.changeSecondCurrency(secondSelect.options[secondSelect.selectedIndex].textContent);
+        ui.changeSecond();
     };
 }
 
 function exchangeCurrency(){
     currency.changeAmount(amountElement.value);    
     currency.exchange()
-        .then(result => console.log(result))
+        .then(result => {
+            ui.displayResult(result);
+        })
         .catch(err => console.log(err));
 }
